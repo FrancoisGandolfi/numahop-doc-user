a5caee705d4ea3395f9826ae7211926342692838
FrancoisGandolfi <115406082+FrancoisGandolfi@users.noreply.github.com>
2026-02-21 21:31:40 +0100

    feat: :closed_lock_with_key: Test Google Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-YQ7G1PJKRW',
+          anonymizeIP: false,
+        },
       }),
     ],
   ],
